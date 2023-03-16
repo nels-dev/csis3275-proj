ac13aa59936b772036eba44a7eaace7dd60b93d9
BrianTsangCa <tyh1004@gmail.com>
2023-03-16 13:05:01 -0700

    changes by lint

--- src/main/java/csis3275/project/seasell/order/OrderController.java
+++ src/main/java/csis3275/project/seasell/order/OrderController.java
@@ -49,6 +49,7 @@ public class OrderController {
         productService.updateProductStatus(orderService.getOrder(orderId).getProduct().getId(), status);
         return ResponseEntity.status(204).build();
     }
+
     // should be like that
     @GetMapping
     public List<OrderDto> getOrders(@RequestParam(required = false) OrderStatus status,
